c29784cbdeec32a46bf4c600bafd89026420fbd4
luxint <meelvoorfer@gmail.com>
2019-06-01 21:16:59 +0200

    simplified extension.js, only evaluating of saved files and deleted evaluating expressions.

---
+++ extension.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const vscode = require("vscode");
+const newlispCommand = 'newlisp';
+const terminalName = 'newLISP REPL';
+const windows = os.platform() == 'win32';
+const pathSeparator = windows ? ';' : ':';
+function newlispExists() {
+    return process.env['PATH'].split(pathSeparator)
+        .some((x) => fs.existsSync(path.resolve(x, newlispCommand + (windows ? '.exe' : ''))));
+}
+function newREPL() {
+    let terminal = vscode.window.createTerminal(terminalName);
+    terminal.sendText(newlispCommand + ' -C', true);
+    vscode.window.withProgress({
+        location: vscode.ProgressLocation.Notification,
+        title: "Running newLISP REPL...",
+        cancellable: false
+    }, (progress, token) => {
+        var p = new Promise(resolve => {
+            setTimeout(() => {
+                terminal.show();
+                thenFocusTextEditor();
+                resolve();
+            }, 2000);
+        });
+        return p;
+    });
+    return terminal;
+}
+function getREPL(show) {
+    let terminal = vscode.window.terminals.find(x => x._name === terminalName);
+    if (terminal == null) {
+        newREPL();
+        return null;
+    }
+    if (show)
+        terminal.show();
+    return terminal;
+}
+
+function thenFocusTextEditor() {
+    setTimeout(() => vscode.commands.executeCommand('workbench.action.focusActiveEditorGroup'), 250);
+}
+function activate(context) {
+    console.log('Extension "vscode-newlisp" is now active!');
+    if (!newlispExists()) {
+        vscode.window.showErrorMessage('Can\'t find newLISP language on your computer! Check your PATH variable.');
+        return;
+    }
+    context.subscriptions.push(vscode.commands.registerCommand('newlisp.startREPL', () => {
+        getREPL(true);
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('newlisp.evalFile', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (editor == null)
+            return;
+        let terminal = getREPL(true);
+        if (terminal == null)
+            return;
+        terminal.sendText('(load ' + '"' + editor.document.fileName + '")',true)
+        thenFocusTextEditor();
+    }));
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
